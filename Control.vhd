b45b5d98f749e8b205a0de30d8a2c06b88b45ae9
Henrique <hss@outlook.com.br>
2020-12-03 23:15:23 -0300

    Control
    Realização do control (máquina de estados)
    
    Co-Authored-By: jamesdievas <48724664+jamesdievas@users.noreply.github.com>

--- Control.vhd
+++ Control.vhd
@@ -1,259 +1,119 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.all;
 
-ENTITY StateMachine IS
-    PORT(   clock, reset                                        :IN STD_LOGIC;
+ENTITY Control IS
+    PORT(   clock1, reset1                                      :IN STD_LOGIC;
             opcode                                              :IN STD_LOGIC_VECTOR(3 DOWNTO 0);
-            PCWrite1, IRWrite1, REGWrite1, ALUSrcA1, RegDst1    :OUT STD_LOGIC;
-            MovtoReg1, ALUSrcB1, ALUOp1                         :OUT STD_LOGIC_VECTOR(1 DOWNTO 0)    
-    );
+            PCWrite1, ALUSrcB1, REGWrite1, RegDst1    			:OUT STD_LOGIC;
+            ALUOp1                         						:OUT STD_LOGIC_VECTOR(1 DOWNTO 0)
+    	);
             
-END StateMachine;
+END Control;
 
-    ARCHITECTURE options OF StateMachine IS
-    TYPE STATE_TYPE IS (Fetch, Decode, OpAdd, OpSub, OpAnd, OpOr, WriteBack);
-    SIGNAL state: STATE_TYPE;
-    --SIGNAL PCWrite, IRWrite, RegDst, ALUScrA, RegWrite : STD_LOGIC;
-    --SIGNAL MovtoReg, ALUOp, ALUScrB : STD_LOGIC_VECTOR(1 DOWNTO 0);
-	--SIGNAL OPCODE : STD_LOGIC_VECTOR(3 DOWNTO 0);
-	BEGIN
-	PROCESS (clock, reset, opcode)
+ARCHITECTURE options OF Control IS
+TYPE STATE_TYPE IS (OpAddR, OpAddI, OpSubR, OpSubI, OpAndR, OpAndI, OpOrR, OpOrI);
+SIGNAL state: STATE_TYPE;
+BEGIN
+	PROCESS (clock1, reset1, opcode)
     BEGIN
+		IF reset1 = '0' THEN
+			PCWrite1 <= '1';
+            --state <= NULL;
+        ELSIF clock1'EVENT AND clock1 = '1' THEN
+            CASE opcode IS  ------ Atribuição do estado atual
+                WHEN "0000"	=> -- MOVR
+                    state <= OpAddR;
 
-        IF reset = '0' THEN
-            state <= Fetch;
-        ELSIF clock'EVENT AND clock = '1' THEN
-            CASE state IS  ------ Atrivuição do próximo estado
-                WHEN Fetch	=>
-                    --CASE OPCODE IS
-                        --WHEN "0000" => state <= Decode;
-                        --WHEN "0010" => state <= Decode;
-                        --WHEN "0011" => state <= Decode;
-                        --WHEN "0100" => state <= Decode; 
-                        --WHEN "0001" => state <= Decode; 
-                        --WHEN "0101" => state <= Decode;
-                        --WHEN "0110" => state <= Decode;
-                        --WHEN "0111" => state <= Decode; 
-                        --WHEN "1000" => state <= Decode;
-                        --WHEN "1001" => state <= Decode;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= Decode;
+                WHEN "0001"	=> -- MOVI
+                    state <= OpAddI;
 
-                WHEN Decode	=>
-                    CASE opcode IS
-                        WHEN "0000" => state <= WriteBack;
-                        WHEN "0001" => state <= WriteBack;
-                        WHEN "0010" => state <= OpAdd;
-                        WHEN "0011" => state <= OpAdd;
-                        WHEN "0100" => state <= OpSub; 
-                        WHEN "0101" => state <= OpSub;
-                        WHEN "0110" => state <= OpAnd;
-                        WHEN "0111" => state <= OpAnd; 
-                        WHEN "1000" => state <= OpOr;
-                        WHEN "1001" => state <= OpOr;
-                        WHEN OTHERS => state <= Fetch;
-                    END CASE;
+                WHEN "0010"	=> -- ADDR
+                    state <= OpAddR;
 
-                WHEN OpAdd	=>
-                    --CASE OPCODE IS
-                        --WHEN "0010" => state <= WriteBack;
-                        --WHEN "0011" => state <= WriteBack;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= WriteBack;
+                WHEN "0011" => -- ADDI
+                    state <= OpAddI;
 
-                WHEN OpSub	=>
-                    --CASE OPCODE IS
-                        --WHEN "0100" => state <= WriteBack;
-                        --WHEN "0101" => state <= WriteBack;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= WriteBack;
+                WHEN "0100" => -- SUBR
+                    state <= OpSubR;
 
-                WHEN OpAnd	=>
-                    --CASE OPCODE IS
-                        --WHEN "0110" => state <= WriteBack;
-                        --WHEN "0111" => state <= WriteBack;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= WriteBack;
+                WHEN "0101" => -- SUBI
+                    state <= OpSubI;
 
-                WHEN OpOr	=>
-                    --CASE OPCODE IS
-                        --WHEN "1000" => state <= WriteBack;
-                        --WHEN "1001" => state <= WriteBack;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= WriteBack;
+                WHEN "0111" => -- ANDR
+					state <= OpAndR;
 
-                WHEN WriteBack	=>
-                    --CASE OPCODE IS
-                        --WHEN "0000" => state <= Fetch;
-                        --WHEN "0001" => state <= Fetch;
-                        --WHEN "0010" => state <= Fetch;
-                        --WHEN "0011" => state <= Fetch;
-                        --WHEN "0100" => state <= Fetch; 
-                        --WHEN "0101" => state <= Fetch;
-                        --WHEN "0110" => state <= Fetch;
-                        --WHEN "0111" => state <= Fetch; 
-                        --WHEN "1000" => state <= Fetch;
-                        --WHEN "1001" => state <= Fetch;
-                        --WHEN OTHERS => state <= Fetch;
-                    --END CASE;
-                    state <= Fetch;
+				WHEN "1000" => -- ANDI
+					state <= OpAndI;
+				
+				WHEN "1000" => -- ORR
+					state <= OpOrR;
+
+				WHEN "1001" => -- ORI
+					state <= OpOrI;
+				
                 WHEN OTHERS => NULL;
             END CASE;
-        END IF;
+		ELSIF clock1'EVENT AND clock1 = '0' THEN
+			CASE state IS
+				WHEN OpAddR => -- Operação de adição tipo R
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '1'; -- Utiliza RD
+					ALUSrcB1 <= '0'; -- Usa registrador
+					ALUOp1 <= "10"; -- Soma
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpAddI => -- Operação de adição tipo I
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '0'; -- Utiliza RT
+					ALUSrcB1 <= '1'; -- Usa imediato
+					ALUOp1 <= "10"; -- Soma
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpSubR => -- Operação de subtração tipo R
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '1'; -- Utiliza RD
+					ALUSrcB1 <= '0'; -- Usa registrador
+					ALUOp1 <= "11"; -- Subtrai
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpSubI => -- Operação de subtração tipo I
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '0'; -- Utiliza RT
+					ALUSrcB1 <= '1'; -- Usa imediato
+					ALUOp1 <= "11"; -- Substrai
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpAndR => -- Operação lógica AND tipo R
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '1'; -- Utiliza RD
+					ALUSrcB1 <= '0'; -- Usa registrador
+					ALUOp1 <= "00"; -- AND
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpAndI => -- Operação Lógica AND tipo I
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '0'; -- Utiliza RT
+					ALUSrcB1 <= '1'; -- Usa imediato
+					ALUOp1 <= "00"; -- AND
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpOrR => -- Operação Lógica OR tipo R
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '1'; -- Utiliza RD
+					ALUSrcB1 <= '0'; -- Usa registrador
+					ALUOp1 <= "01"; -- OR
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OpOrI => -- Operação Lógica OR tipo I
+					PCWrite1 <= '1'; -- PCWrite sempre ativo
+					RegDst1 <= '0'; -- Utiliza RT
+					ALUSrcB1 <= '1'; -- Usa imediato
+					ALUOp1 <= "01"; -- OR
+					RegWrite1 <= '1'; -- Sempre ativo
+
+				WHEN OTHERS => NULL;
+			END CASE;
+		END IF;
 	END PROCESS;
-    -------------------------------------------------------------------------
-    --Atribuição dos sinais do estado atual
-	 PROCESS(state, opcode,reset)
-	 BEGIN
-		CASE state IS
-			WHEN Fetch => -- operação de Fetch não importa o valor de opcode (independência de tipos)
-				PCWrite1 <= '1';
-				IRWrite1 <= '1';
-				RegDst1 <= '0';
-				MovtoReg1 <= "00";
-				ALUSrcA1 <= '0';
-				ALUSrcB1 <= "01";
-				ALUOp1 <= "10";
-				RegWrite1 <= '0';
-				
-			WHEN Decode => -- operação de Decode não importa o valor de opcode (independência de tipos)
-				PCWrite1 <= '0';
-				IRWrite1 <= '0';
-				RegDst1 <= '0';
-				MovtoReg1 <= "00";
-				ALUSrcA1 <= '0';
-				ALUSrcB1 <= "00";
-				ALUOp1 <= "00";
-				RegWrite1 <= '0';
-				
-			WHEN OpAdd => -- operação de adição depende dos tipos 
-				IF opcode(0) = '1' THEN -- para tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "10";
-					ALUOp1 <= "10";
-					RegWrite1 <= '0';
-				ELSE -- para tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "00";
-					ALUOp1 <= "10";
-					RegWrite1 <= '0';
-				END IF;
-				
-			WHEN OpSub => -- operação de subtração depende dos tipos 
-				IF opcode(0) = '1' THEN -- para tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "10";
-					ALUOp1 <= "11";
-					RegWrite1 <= '0';
-				ELSE -- para tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "00";
-					ALUOp1 <= "11";
-					RegWrite1 <= '0';
-				END IF;
-				
-			WHEN OpAnd => -- operação de AND depende dos tipos 
-				IF opcode(0) = '1' THEN -- para tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "10";
-					ALUOp1 <= "00";
-					RegWrite1 <= '0';
-				ELSE -- para tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "00";
-					ALUOp1 <= "00";
-					RegWrite1 <= '0';
-				END IF;
-				
-			WHEN OpOr => -- operação de OR depende dos tipos 
-				IF opcode(0) = '1' THEN -- para tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "10";
-					ALUOp1 <= "01";
-					RegWrite1 <= '0';
-				ELSE -- para tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					ALUSrcA1 <= '1';
-					ALUSrcB1 <= "00";
-					ALUOp1 <= "01";
-					RegWrite1 <= '0';
-				END IF;
-			WHEN WriteBack => -- WriteBack depende dos tipos para a escrita no banco de registradores
-				IF opcode = "0001" THEN -- para move tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "01";
-					--ALUSrcA1 <= '1';
-					--ALUSrcB1 <= "10";
-					--ALUOp1 <= "00";
-					RegWrite1 <= '1';
-				ELSIF opcode = "0000" THEN -- para move tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '1';
-					MovtoReg1 <= "10";
-					--ALUSrcA1 <= '1';
-					--ALUSrcB1 <= "00";
-					--ALUOp1 <= "00";
-					RegWrite1 <= '1';
-				ELSIF opcode(0) = '0' THEN -- para operações de ALU tipo R
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '1';
-					MovtoReg1 <= "00";
-					--ALUSrcA1 <= '1';
-					--ALUSrcB1 <= "00";
-					--ALUOp1 <= "00";
-					RegWrite1 <= '1';
-				ELSIF opcode(0) = '1' THEN -- para operações de ALU tipo I
-					PCWrite1 <= '0';
-					IRWrite1 <= '0';
-					RegDst1 <= '0';
-					MovtoReg1 <= "00";
-					--ALUSrcA1 <= '1';
-					--ALUSrcB1 <= "00";
-					--ALUOp1 <= "00";
-					RegWrite1 <= '1';
-				END IF;
-			WHEN OTHERS => NULL;
-		END CASE;
-END PROCESS;
 END options;
     
